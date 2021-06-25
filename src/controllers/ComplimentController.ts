9ceffcfbf3772c51461691478d857c8c58cf3c15
Bruno Lopes <eusoubruno7@gmail.com>
2021-06-25 14:20:25 -0300

    feat: adjust in ComplimentController

--- src/controllers/ComplimentController.ts
+++ src/controllers/ComplimentController.ts
@@ -3,16 +3,17 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 
 class ComplimentController {
   public async store(request: Request, response: Response) {
-    const { tag_id, user_sender, user_receiver, message } = request.body;
+    const { tag_id, user_receiver, message } = request.body;
+    const user_sender = request.user_id;
     const createComplimentService = new CreateComplimentService();
 
-    const user = await createComplimentService.execute({
+    const compliment = await createComplimentService.execute({
       tag_id,
       user_sender,
       user_receiver,
       message,
     });
-    return response.json(user);
+    return response.json(compliment);
   }
 }
 
